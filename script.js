85af572bfa34c7180711811b81de112aaf9be964
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-15 09:53:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-
+	event.preventDefault();
+	let forms = document.getElementById("form1");
+	let firstname = forms["fname"].value;
+	let lastname = forms["lname"].value;
+alert(`${firstname} ${lastname}`);
 }
